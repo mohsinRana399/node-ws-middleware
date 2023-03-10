48aff09ec4c4d28978b4539c5404041cf70aa121
mohsinRana399 <battleaxe399@gmail.com>
2023-03-10 16:24:36 +0500

    updates

--- utils/handleSockets.js
+++ utils/handleSockets.js
@@ -5,15 +5,19 @@ const { SOCKET_EVENTS } = require("./constants");
 const socketServer = () => {
   const wsServer = new ws.Server({ noServer: true });
 
-  wsServer.on("connection", (socket) => {
-    const djangoSocket = new ws("ws://192.168.18.68:8000/notification");
+  wsServer.on("connection", (socket, req) => {
+    const _url = req?.url?.split("=")[1];
+    console.log({ _url });
 
+    const djangoSocket = new ws(
+      `ws://192.168.18.68:8000/notification?token=${_url}`
+    );
     djangoSocket.on("message", (data) => {
-      socket.send(`From Django-> ${data}`);
+      socket.send(`${data}`);
     });
     socket.on(SOCKET_EVENTS.MESSAGE_RECEIVED, (message) => {
       //Forward to django
-      console.log("sending", { message }, "To django");
+      // console.log("sending", { message }, "To django");
       djangoSocket.send(message);
     });
   });
