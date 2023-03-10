89ad02c2c27ec95884206afc8d28a8dd04d2d182
mohsinRana399 <battleaxe399@gmail.com>
2023-03-10 16:42:10 +0500

    updates

--- utils/handleSockets.js
+++ utils/handleSockets.js
@@ -3,24 +3,27 @@ const ws = require("ws");
 const { SOCKET_EVENTS } = require("./constants");
 
 const socketServer = () => {
-  const wsServer = new ws.Server({ noServer: true });
+  try {
+    const wsServer = new ws.Server({ noServer: true });
 
-  wsServer.on("connection", (socket, req) => {
-    const _url = req?.url?.split("=")[1];
-    console.log({ _url });
+    wsServer.on("connection", (socket, req) => {
+      const _token = req?.url?.split("=")[1];
+      const djangoSocket = new ws(
+        `${process.env.DJANGO_SERVER_URL}?token=${_token}`
+      );
+      djangoSocket?.on("message", (data) => {
+        socket.send(`${data}`);
+      });
+      socket.on(SOCKET_EVENTS.MESSAGE_RECEIVED, (message) => {
+        //Forward to django
+        // console.log("sending", { message }, "To django");
+        djangoSocket?.send(message);
+      });
+    });
 
-    const djangoSocket = new ws(
-      `${process.env.DJANGO_SERVER_URL}?token=${_url}`
-    );
-    djangoSocket.on("message", (data) => {
-      socket.send(`${data}`);
-    });
-    socket.on(SOCKET_EVENTS.MESSAGE_RECEIVED, (message) => {
-      //Forward to django
-      // console.log("sending", { message }, "To django");
-      djangoSocket.send(message);
-    });
-  });
-  return wsServer;
+    return wsServer;
+  } catch (error) {
+    console.log({ error });
+  }
 };
 module.exports = { socketServer };
